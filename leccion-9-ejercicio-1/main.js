8956c3e22c4012efc875375d96dfbac8d11a4f8c
alicia-colom <aliciacolom@gmail.com>
2020-10-15 11:20:34 +0200

    Exercise 1 from lesson 2.9

---
+++ leccion-9-ejercicio-1/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//EJERCICIO 1
+
+const numberList = [];
+let number = 0;
+
+for (let i = 0; i < 100; i++) {
+	number = number + 1;
+	console.log("esto es number: " + number);
+	numberList.push(number);
+}
+
+console.log("esto es el array: " + numberList);
+
+// EJERCICIO 2
