617eb7f06a40b1f78aa0d7140a4d645227be5ed9
alicia-colom <aliciacolom@gmail.com>
2020-10-21 11:48:09 +0200

    Add exercise 9 but with a problem in the second part

--- leccion-13-ejercicios/main.js
+++ leccion-13-ejercicios/main.js
@@ -3,11 +3,11 @@
 //EJERCICIO 9
 
 const runners = [
-	{ name: "Gregory Goyle", time: 56, student: true },
+	{ name: "Gregory Goyle", time: 66, student: true },     ////
 	{ name: "Nymphadora Tonks", time: 9, student: false },
-	{ name: "Luna Lovegood", time: 45, student: true },
+	{ name: "Luna Lovegood", time: 26, student: true },    ////
 	{ name: "Cedric Diggory", time: 28, student: false },
-	{ name: "Cho Chang", time: 35, student: true },
+	{ name: "Cho Chang", time: 35, student: true },        ////
 ];
 
 //filter por estudiante
@@ -26,15 +26,25 @@ console.log("esto es el array studentRunners: ", studentRunners);
 // reduce de studentRunners
 
 function winner(faster, runner) {
-	// debugger;
+	debugger;
 	//comparar la propiedad TIME para sacar el menor valor
 	if (runner.time > faster) {
-		console.log("entro en el IF de winner");
+    console.log("entro en el IF de winner");
+
+    console.log("soy faster: ", faster);
+    console.log("soy runner: ", runner);
 		return faster;
 	} else {
+    console.log("entro en el ELSE de winner");
 		return runner;
 	}
 }
 
 const studentWinner = studentRunners.reduce(winner);
 console.log("esto es el array studentWinner: ", studentWinner);
+
+//error: el tiempo de ChoChang es mayor que el tiempo de Luna
+// es decir: 35 > 26
+// por lo que debería entrar por el IF
+// PERO entra por el ELSE
+// ¿¿ pq ??
