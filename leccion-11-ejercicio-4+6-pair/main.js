9269b30f94d370e7d899d627bb6ed38a287c657e
nuriadiazcandela <nuriadiazcandela@gmail.com>
2020-10-22 10:55:33 +0200

    Ex 4-6 leccion 11

--- leccion-11-ejercicio-4+6-pair/main.js
+++ leccion-11-ejercicio-4+6-pair/main.js
@@ -1,31 +1,52 @@
-"use strict";
+'use strict';
 
 // EJERCICIO 4 + 6
 // 1.- evento keyup en el input --> string en el p + localstorage
 // 2.- review localstorage --> recargar datos
 // 3.- datos en objeto + stringify
 
-const nameBox = document.querySelector(".js-name");
-const surnameBox = document.querySelector(".js-surname");
-const paragraph = document.querySelector(".js-paragraph");
-const userData = [{ name: "" }, { surname: "" }];
+const nameBox = document.querySelector('.js-name');
+const surnameBox = document.querySelector('.js-surname');
+const paragraph = document.querySelector('.js-paragraph');
+let userData = { userName: 'nombre', userSurname: 'apellido' };
+let name = nameBox.value;
+let surname = surnameBox.value;
+const btn = document.querySelector('.js-btn');
 
 // NO TENEMOS CLARO CÓMO USAR A CONTINUACIÓN EL LOCALSTORAGE:
 // userData = localStorage.getItem("fullname");
 
 function handleGetData(event) {
-	paragraph.innerHTML = nameBox.value + " " + surnameBox.value;
+  paragraph.innerHTML = nameBox.value + ' ' + surnameBox.value;
 
-	if (event.currentTarget.id === "name") {
-		userData.name = event.currentTarget.value;
-	} else {
-		userData.surname = event.currentTarget.value;
-	}
-
-	console.log(userData);
-
-	localStorage.setItem("fullName", userData);
+  if (event.currentTarget.id === 'name') {
+    userData.userName = event.currentTarget.value;
+  } else {
+    userData.userSurname = event.currentTarget.value;
+  }
+  storeLocal();
 }
 
-nameBox.addEventListener("keyup", handleGetData);
-surnameBox.addEventListener("keyup", handleGetData);
+nameBox.addEventListener('keyup', handleGetData);
+surnameBox.addEventListener('keyup', handleGetData);
+
+// ESTO ES PARA GUARDAR LOS DATOS:
+
+function storeLocal() {
+  const jsonUserData = JSON.stringify(userData);
+  localStorage.setItem('fullname', jsonUserData);
+}
+
+// RECUPERAR DATOS:
+
+function getLocal() {
+  const storeUserData = localStorage.getItem('fullname');
+  const lastData = JSON.parse(storeUserData);
+  userData = lastData;
+}
+function repintName() {
+  getLocal();
+  nameBox.value = userData.userName;
+  surnameBox.value = userData.userSurname;
+}
+btn.addEventListener('click', repintName);
